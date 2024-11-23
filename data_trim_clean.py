6d4cea12797a4ad32443b020d76678b4420b3c9c
Danish Munib <danishmunibcontact@gmail.com>
2024-11-23 16:31:12 +0500

    Update data_trim_clean.py
    added a new comment

--- data_trim_clean.py
+++ data_trim_clean.py
@@ -18,23 +18,23 @@ data = data[data.apply(lambda row: all(len(val) > 0 if isinstance(val, list) els
 
 #function to trim the "authors" value to just the names
 def clean_authors(authors):
-    # Iterate over each author and remove unwanted keys
+    # Iterate over each author and unwanted columns
     for author in authors:
-        author.pop("id", None)  # Remove 'id' if exists
-        author.pop("org", None)  # Remove 'org' if exists
+        author.pop("id", None) 
+        author.pop("org", None) 
     return authors
 
 data['authors'] = data['authors'].apply(clean_authors)
 
 #function to convert the name: "name" pair to a list of author names
 def convert_authors_to_list(authors):
-    # Flatten the authors' dictionaries to a list of all their values (e.g., 'id', 'name', 'org')
     author_list = []
     for author in authors:
-        author_list.extend(list(author.values()))  # Add all values from the author dictionary to the list
+        author_list.extend(list(author.values())) 
     return author_list
 
 data['authors'] = data['authors'].apply(convert_authors_to_list)
 
 
+#input the new name here
 data.to_json('CleanDataSet.json', orient = 'records', indent=4)
