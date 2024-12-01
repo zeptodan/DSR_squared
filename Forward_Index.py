b3186aa57785fc65c7e260c2a28620a9adf2ddec
zeptodan <muhammadannas138@gmail.com>
2024-12-01 17:39:37 +0500

    added comments for inverted and forward index

--- Forward_Index.py
+++ Forward_Index.py
@@ -5,6 +5,7 @@ import json
 #load lexicon as dictionary
 lexi=pd.read_csv("Lexicon.csv",names=["count","word","id"])
 lexicon=lexi.set_index("word").to_dict(orient="index")
+
 #open dataset file and file to write in
 file=open("1000_clean_dataset.json","r")
 writefile=open("Forward_index.json","w")
@@ -14,11 +15,13 @@ document_index={}
 i=1
 count=0
 writefile.write("[")
+#iterate over each document in dataset
 for document in documents:
     print("processing " + str(i))
     count=0
     if i!=1:
         writefile.write(",")
+    #store metadata of document 
     document_index["id"]=document["id"]
     document_index["off"]=writefile.tell()
     document_index["cite"]=document["n_citation"]
@@ -26,35 +29,47 @@ for document in documents:
     authors=[author["name"] for author in document["authors"]]
     document_index["authors"]=authors
     i+=1
+    #iterate over words in title
     title=nlp(''.join(document["title"]))
     count+=len(title)
     for word in title:
         wordlemma=word.lemma_.lower()
+        #if word is not in document_index add it
         if wordlemma in lexicon and lexicon[wordlemma]["id"] not in document_index:
             document_index[lexicon[wordlemma]["id"]] = {"T":1,"K":0,"A":0}
+        #otherwise if it is already there increment its count by 1
         elif wordlemma in lexicon:
             document_index[lexicon[wordlemma]["id"]]["T"]+=1
             
+    #iterate over words in keywords
     keywords=nlp(''.join(document["keywords"]))
     count+=len(keywords)
     for word in keywords:
         wordlemma=word.lemma_.lower()
+        #if word is not in document_index
         if wordlemma in lexicon and lexicon[wordlemma]["id"] not in document_index:
             document_index[lexicon[wordlemma]["id"]] = {"T":0,"K":1,"A":0}
+        #otherwise if it is already there increment its count by 1
         elif wordlemma in lexicon:
             document_index[lexicon[wordlemma]["id"]]["K"]+=1
             
+    #iterate over words in abstract
     abstract=nlp(''.join(document["abstract"])) 
     count+=len(abstract)
     for word in abstract:
         wordlemma=word.lemma_.lower()
+        #if word is not in document_index
         if wordlemma in lexicon and lexicon[wordlemma]["id"] not in document_index:
             document_index[lexicon[wordlemma]["id"]] = {"T":0,"K":0,"A":1}
+        #otherwise if it is already there increment its count by 1
         elif wordlemma in lexicon:
             document_index[lexicon[wordlemma]["id"]]["A"]+=1
     document_index["len"]=count
+    #write the index created for the document in file
     json.dump(document_index,writefile,separators=(',',':'))
+    #clear the index for the next document
     document_index.clear()
+#close the list and then the files
 writefile.write("]")
 writefile.close()
 file.close()
--- Inverted_Index.py
+++ Inverted_Index.py
@@ -1,6 +1,6 @@
 import ijson
 import json
-
+#function to give score to words in the document based on its importance
 def calculate_weight(value,length):
     return (value["T"]+value["K"]+value["A"])/length
     
@@ -10,15 +10,21 @@ writefile=open("Inverted_index.json","w")
 documents=ijson.items(file,"item")
 inverted_index={}
 i=1
+#iterate over the forward index
 for document in documents:
     print("processing " + str(i))
     i+=1
     DocID=document["id"]
     offset=document["off"]
+    #iterate over the key value pairs in the document
     for key,value in document.items():
+        #ignore if it is not a word id
         if key not in ["id","authors","cite","len","off","url"]:
+            #if the word is not in inverted index
+            #for each word store list of document id, its weight and documents offset in forward index
             if key not in inverted_index:
                 inverted_index[key]=[[DocID,calculate_weight(value,document["len"]),offset]]
+            #if it is already there just append the list of documents where that word appears
             else:
                 inverted_index[key].append([DocID,calculate_weight(value,document["len"]),offset])
 json.dump(inverted_index,writefile,separators=(',',':'))
