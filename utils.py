13a50b1ef58b2e8c68f0b870713cd293cc0a132f
Danish Munib <danishmunibcontact@gmail.com>
2024-12-22 17:23:03 +0500

    modified structure for correct loading

--- utils.py
+++ utils.py
@@ -2,14 +2,21 @@ import pandas
 import faiss
 import numpy
 
-Lexicon = pandas.read_csv("Lexicon.csv", usecols=[2])
-words= Lexicon.iloc[:, 0].tolist()
-word_count = len(words)
-embeddings = numpy.load(".\\semantic\\vectors.npy")
-FAISS = faiss.read_index(".\\semantic\\Faiss_index.index")
+
+loaded = False
 
 # Function to find similar words
 def find_matches(wordID, k):
+    global loaded
+
+    if not loaded:
+        Lexicon = pandas.read_csv("Lexicon.csv", usecols=[2])
+        words= Lexicon.iloc[:, 0].tolist()
+        word_count = len(words)
+        embeddings = numpy.load(".\\semantic\\vectors.npy")
+        FAISS = faiss.read_index(".\\semantic\\Faiss_index.index")
+        loaded = True
+    
     if wordID<0 or wordID>word_count:
         return []
 
