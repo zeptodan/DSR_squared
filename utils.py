c0cc6f49ae35a69beab263dfeb0acd046215962c
Danish Munib <danishmunibcontact@gmail.com>
2024-12-25 23:33:12 +0500

    Changed utils structure find_matches now returns words

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 import pandas
 import faiss
 import numpy
+import csv
 
 
 loaded = False
 word_count = 0
 embeddings = None
+FAISS = None
+lexicon = {}
 
-# Function to find similar words
-def find_matches(wordID, k):
+def resource_loader():
     global loaded
     global word_count
     global embeddings
+    global FAISS
+    global lexicon
 
     if not loaded:
         Lexicon = pandas.read_csv("Lexicon_small.csv", usecols=[2])
@@ -19,14 +23,38 @@ def find_matches(wordID, k):
         word_count = len(words)
         embeddings = numpy.load("vectors.npy")
         FAISS = faiss.read_index("Faiss_index.index")
+        with open("LexiconFinal.csv") as f:
+            file = csv.reader(f)
+            for row in file:
+                word = row[1]
+                stuff = [int(row[2]), int(row[0])]
+                lexicon[word] = stuff
         loaded = True
-    
-    if wordID<0 or wordID>word_count:
-        return []
+
+# Function to find similar words
+def find_matches(word, k):
+    global word_count
+    global words
+    global embeddings
+    global FAISS
+    global lexicon
+
+    wordID = lexicon[word][1]
+    if wordID<1 or wordID>word_count:
+        return {}
 
     word_embedding = embeddings[wordID].reshape(1, -1)
     distances, indices = FAISS.search(word_embedding, k)
 
+    matches = {}
+
+    for ID, distance in zip(indices[0], distances[0]):
+        matches[words[ID]] = distance
+    
+    return matches
+
+    """
     # Retrieve similar words and their distances
     similar_words = list(zip([i for i in indices[0]], distances[0]))
     return similar_words
+    """
