1e12db8ca41626bf54ca741ff77ffc0bd04d76ba
zeptodan <muhammadannas138@gmail.com>
2024-12-26 17:44:46 +0500

    trying

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import pandas as pd
-import spacy
 from documentRetrieval import search
 import time
 dataset=r"TheCleanData2.0"
+
 def main():
-    lexi = pd.read_csv("Lexi_clusters.csv", names=["count", "word", "id","barrel"],dtype={"count":int, "word":str, "id":str,"barrel":str}, keep_default_na=False, na_values=[])
-    lexicon=lexi.set_index("word").to_dict(orient="index")
-    nlp = spacy.load("en_core_web_md")
+    # resource_loader()
     query = input("enter: ")
     start = time.time()
-    docs_to_load = search(query,lexicon,nlp)
+    docs_to_load = search(query)
     print("time for whole search: " + str(time.time() - start))
     docs=getDocs(docs_to_load)
     print("top result: ")
@@ -39,7 +36,8 @@ def getDocs(docs_to_load):
     print("getting docs from dataset: " + str(time.time() - start))
     return docs
         
-        
+          
+ 
 if __name__ == "__main__":
     main()
         
--- ranking.py
+++ ranking.py
@@ -1,8 +1,6 @@
 import math
-from multiprocessing import Pool
-import timeit
 def rank_similar_words(words_and_docs, similarity, lexicon, total_docs,resultant_docs):
-    
+    print("nnnn")
     for word,docs in words_and_docs.items():
         doc_count=lexicon[word]['count']
         idf=math.log(total_docs/doc_count)
@@ -13,17 +11,15 @@ def rank_similar_words(words_and_docs, similarity, lexicon, total_docs,resultant
             else:
                 resultant_docs[doc[0]] = score
 
-def main():
-    global lexicon, words_and_docs, TOTAL_DOCS, similarity,resultant_docs
-    lexicon={'hello':(0,2),'world':(1,3),'python':(2,4)}
-    words_and_docs={'hello':[[69,1,12]],'world':[[70,.5,2]]}
-    TOTAL_DOCS=4040997
-    resultant_docs={}
+# def main():
+#     global lexicon, words_and_docs, TOTAL_DOCS, similarity,resultant_docs
+#     lexicon={'hello':(0,2),'world':(1,3),'python':(2,4)}
+#     words_and_docs={'hello':[[69,1,12]],'world':[[70,.5,2]]}
+#     TOTAL_DOCS=4040997
+#     resultant_docs={}
 
-    similarity={'hello':1,'world':2,'python':3}
-    execution_time = timeit.timeit(lambda: rank_similar_words(words_and_docs, similarity, lexicon, TOTAL_DOCS,resultant_docs), number=1)
-    print(f"--- {execution_time * 1_000:.7f} milliseconds ---")
-    print(resultant_docs)
+#     similarity={'hello':1,'world':2,'python':3}
+#     print(resultant_docs)
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
